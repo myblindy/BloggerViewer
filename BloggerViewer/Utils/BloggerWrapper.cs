320eeff1576d636a506f980f5b59490cbe65e3dd
Marius Mitea <tweety.04.01@gmail.com>
2016-06-29 03:35:26 -0400

    Add project files.

---
+++ BloggerViewer/Utils/BloggerWrapper.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Blogger.v3;
+using Google.Apis.Blogger.v3.Data;
+using Google.Apis.Services;
+
+namespace BloggerViewer.Utils
+{
+    public class PostWrapper
+    {
+        public string Title { get; set; }
+        public DateTime Published { get; set; }
+        public IEnumerable<string> Images { get; set; }
+    }
+
+    public class BloggerWrapper
+    {
+        private static BloggerService Service;
+
+        public static async Task AuthenticateAsync()
+        {
+            var credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                Application.GetResourceStream(new Uri("pack://application:,,,/client_id.json")).Stream,
+                new[] { BloggerService.Scope.BloggerReadonly },
+                "user",
+                CancellationToken.None);
+
+            Service = new BloggerService(new BaseClientService.Initializer
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "BloggerQuickViewer",
+            });
+        }
+
+        public static async Task<Blog> GetBlogAsync() => await Service.Blogs.GetByUrl(@"https://1z2x1z-apink.blogspot.ca/").ExecuteAsync();
+
+        public static async Task<IEnumerable<PostWrapper>> GetPostsAsync()
+        {
+            var blog = await GetBlogAsync();
+            var listrequest = Service.Posts.List(blog.Id);
+            listrequest.MaxResults = 500;
+
+            var lst = new List<PostWrapper>();
+            while (true)
+            {
+                var newposts = await listrequest.ExecuteAsync();
+                lst.AddRange(newposts.Items.Select(p => new PostWrapper
+                {
+                    Title = p.Title,
+                    Published = p.Published.Value,
+                    Images = GetImageLinksFromContent(p.Content)
+                }));
+
+                if (newposts.Items.Count == 0 || string.IsNullOrWhiteSpace(newposts.NextPageToken))
+                    break;
+
+                listrequest.PageToken = newposts.NextPageToken;
+            }
+
+            return lst;
+        }
+
+        private static IEnumerable<string> GetImageLinksFromContent(string content)
+        {
+            var doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(content);
+            var nodes = doc.DocumentNode.SelectNodes(".//img");
+            return nodes == null ? new string[0] : nodes.Select(n => n.Attributes["src"].Value).ToArray();
+        }
+    }
+}
---
+++ BloggerViewer/Utils/ValueConverters.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using Google.Apis.Blogger.v3.Data;
+
+namespace BloggerViewer.Utils
+{
+    public class PostToListItemConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var p = (PostWrapper)value;
+            return p.Published.ToString("[yyMMdd] ") + p.Title;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
